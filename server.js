35857a7b6e9edb3884401e389010c0c9726a8d41
24HP1005 24HP1005 <24HP1005@admins-iMac.local>
2024-12-06 19:04:31 -0800

    uurchlult

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors')
+const dotenv = require('dotenv')
 const userRoute = require("./routes/userRoutes");
 const postRoute = require("./routes/postRoutes");
 const commentRoute = require("./routes/commentRoutes");
-
-
-const connectDatabase = async () => {
-    const res = await mongoose.connect(
-        "mongodb+srv://Ariyaa:9JQLts4SGLYoGiUa@cluster0.9qig3.mongodb.net/instagram?retryWrites=true&w=majority&appName=Cluster0"
-    );
-    if (res) console.log("db connected");
-};
-
-connectDatabase();
-
+dotenv.config()
 
 const app = express();
 const PORT = 8080;
+app.use(cors())
 app.use(express.json());
 
 
 app.use(userRoute);
 app.use(postRoute);
 app.use(commentRoute);
+
+
+
+const connectDatabase = async () => {
+    const res = await mongoose.connect(process.env.MONGODB_URI);
+    if (res) console.log("db connected");
+};
+
+connectDatabase();
 
 app.listen(PORT, () => {
     console.log(`your server is running on ${PORT}`);
